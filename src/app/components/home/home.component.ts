afc9dfe8818830b4b2528b7f95372faebc6f444b
CharithSuriyakula <charith.suriyakula@conxillium.com>
2021-10-27 12:04:51 +0530

    adding login check, google analytics

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,12 +4,16 @@ import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { SharedService } from 'src/app/services/data/shared.service';
 
+import data from './../../../assets/data.json';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  users = [];
 
   name = new FormControl(null, [Validators.required]);
   email = new FormControl(null, [Validators.required]);
@@ -30,6 +34,7 @@ export class HomeComponent implements OnInit {
 
   }
   ngOnInit(): void {
+    this.users = data["users"];
     this.checkOrientation();
   }
 
@@ -45,23 +50,42 @@ export class HomeComponent implements OnInit {
     }
   }
 
+  /**
+   * Bind to orientation changes in a mobile device
+   */
   @HostListener('window:orientationchange')
   orientationChanged(): void {
     this.checkOrientation();
   }
 
+  /**
+   * Login click
+   */
   login(): void {
     // validate
     if (this.name.valid && this.email.valid && this.accessCode.valid) {
-      if (this.accessCode.value.startsWith("ACE")) {
-        // this.sharedDataSerive.liveUrl = liveUrl;
-        this.sharedDataSerive.name = this.name.value;
 
-        // navigate to tour
-        this.router.navigate(['tour']);
+      let user = this.users.filter(x => x["name"] === this.email.value);
+      if (user.length > 0) {
+        // this.accessCode.value.startsWith("ACE")
+        if (this.accessCode.value.trim() === user[0]["accessCode"]) {
+          this.sharedDataSerive.name = this.name.value;
+
+          // navigate to tour
+          this.router.navigate(["tour"]);
+        }
+        else {
+          // invalid pw
+          this.modalService.open("Invalid Credentials", { modalDialogClass: "text-center p-4" });
+        }
       }
+      else {
+        // no user found
+        this.modalService.open("User not found", { modalDialogClass: "text-center p-4" });
+      }
+
     } else {
-      this.modalService.open("Invalid Credentials", { modalDialogClass: "text-center p-4" });
+      this.modalService.open("Please fill data", { modalDialogClass: "text-center p-4" });
     }
   }
 }
