418242bdf81683e7fde859247d9e376d3b82c64f
xwpdev <charith.suriyakula@gmail.com>
2021-10-31 11:59:00 +0530

    refactoring

--- src/app/components/tour/tour.component.ts
+++ src/app/components/tour/tour.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { DeviceDetectorService } from 'ngx-device-detector';
 import { SharedService } from 'src/app/services/data/shared.service';
+import { Constants } from 'src/app/shared/constants';
 
 @Component({
   selector: 'app-tour',
@@ -23,21 +24,17 @@ export class TourComponent implements OnInit {
     const currentDevice = this.deviceDetectorService.getDeviceInfo();
     const browserVersion = parseFloat(currentDevice.browser_version);
 
-    let liveUrl = 'https://virtualpodiumtest2.z23.web.core.windows.net/';
-
-    // liveUrl = (currentDevice.os === "iOS" && currentDevice.browser === "Safari")
-    //   ? 'googlechromes://virtualpodiumtest.z23.web.core.windows.net/'
-    //   : 'https://virtualpodiumtest2.z23.web.core.windows.net/'
+    let liveUrl = Constants.WEBGL_2;
 
     if (currentDevice.os === "iOS" && currentDevice.browser === "Safari") {
       if (browserVersion < 15)
-        liveUrl = 'googlechromes://virtualpodiumtest.z23.web.core.windows.net/'
+        liveUrl = Constants.CHROME_LINK;
     }
 
     setTimeout(() => {
       if (currentDevice.os === "iOS" && currentDevice.browser === "Safari") {
         if (browserVersion < 15)
-          setTimeout(function () { window.location.href = "https://apps.apple.com/us/app/google-chrome/id535886823"; }, 500);
+          setTimeout(function () { window.location.href = Constants.APP_STORE_CHROME; }, 500);
       }
 
       window.location.href = liveUrl;
---
+++ src/app/shared/constants.ts
@@ -0,0 +1,11 @@
+export class Constants {
+  static EVENT_NAME = 'ace2021'
+
+  static WEBGL_1 = `https://${Constants.EVENT_NAME}wgl1.z23.web.core.windows.net/`;
+
+  static WEBGL_2 = `https://${Constants.EVENT_NAME}wgl2.z23.web.core.windows.net/`;
+
+  static CHROME_LINK = `googlechromes://${Constants.EVENT_NAME}wgl1.z23.web.core.windows.net/`;
+
+  static APP_STORE_CHROME = 'https://apps.apple.com/us/app/google-chrome/id535886823';
+}
