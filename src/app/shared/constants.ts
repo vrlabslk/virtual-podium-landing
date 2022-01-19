6c265fca5fff1de6cbb9e2134aa30e4a8b2af6da
xwpdev <charith.suriyakula@gmail.com>
2022-01-19 20:36:19 +0530

    updates

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,13 +1,14 @@
 export class Constants {
-  static EVENT_NAME = 'slendo2021'
+  static EVENT_NAME = 'coaisl2022'
 
   static EVENT_TITLE = '';
 
-  static WEBGL_1 = `https://${Constants.EVENT_NAME}wgl1.z23.web.core.windows.net/`;
+  static WEBGL_1 = `https://${Constants.EVENT_NAME}wgl1.z13.web.core.windows.net/`;
+  // https://coaisl2022wgl2.z13.web.core.windows.net/
 
-  static WEBGL_2 = `https://${Constants.EVENT_NAME}wgl2.z23.web.core.windows.net/`;
+  static WEBGL_2 = `https://${Constants.EVENT_NAME}wgl2.z13.web.core.windows.net/`;
 
-  static CHROME_LINK = `googlechromes://${Constants.EVENT_NAME}wgl1.z23.web.core.windows.net/`;
+  static CHROME_LINK = `googlechromes://${Constants.EVENT_NAME}wgl1.z13.web.core.windows.net/`;
 
   static APP_STORE_CHROME = 'https://apps.apple.com/us/app/google-chrome/id535886823';
 
