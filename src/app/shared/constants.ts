d42188f6292c90fbdc793ed30aed747a0354fa2a
CharithSuriyakula <charith.suriyakula@conxillium.com>
2022-01-21 09:42:51 +0530

    storage account updates

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,16 +1,18 @@
 export class Constants {
-  static EVENT_NAME = 'coaisl2022'
+  static EVENT_NAME = 'coaisl2022';
 
   static EVENT_TITLE = '';
 
-  static WEBGL_1 = `https://${Constants.EVENT_NAME}wgl1.z13.web.core.windows.net/`;
+  static WEBGL_1 = `https://${Constants.EVENT_NAME}wgl10.z23.web.core.windows.net/`;
   // https://coaisl2022wgl2.z13.web.core.windows.net/
 
-  static WEBGL_2 = `https://${Constants.EVENT_NAME}wgl2.z13.web.core.windows.net/`;
+  static WEBGL_2 = `https://${Constants.EVENT_NAME}wgl20.z23.web.core.windows.net/`;
 
   static CHROME_LINK = `googlechromes://${Constants.EVENT_NAME}wgl1.z13.web.core.windows.net/`;
 
-  static APP_STORE_CHROME = 'https://apps.apple.com/us/app/google-chrome/id535886823';
+  static APP_STORE_CHROME =
+    'https://apps.apple.com/us/app/google-chrome/id535886823';
 
-  static PLAY_STORE_CHROME = 'https://play.google.com/store/apps/details?id=com.android.chrome&hl=en&gl=US';
+  static PLAY_STORE_CHROME =
+    'https://play.google.com/store/apps/details?id=com.android.chrome&hl=en&gl=US';
 }
